45789c0472bde42528853b9d27b50561e83d6104
haisyamm <70573490+haisyamm@users.noreply.github.com>
2021-09-15 14:19:52 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,23 @@
+<?php 
+$nama = "Unyil";
+$alamat = "TRANS 7";
+$nohp = "0811111111";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Week 1 - Usro</title>
+</head>
+<body>
+	<?php 
+		echo "BIODATA";
+		echo "<br><br>"; //Untuk memberikan spasi
+
+		echo "Nama 		:".$nama."Unyil".$alamat;
+		echo "<br>";
+		echo "Alamat 	:".$alamat;
+		echo "<br>";
+		echo "NO HP 	:".$nohp;
+	?>
+</body>
+</html>
