adde8990ef3e107c5833b60c217432db7685f877
haisyamm <70573490+haisyamm@users.noreply.github.com>
2021-12-15 13:58:41 +0700

    Create login.php

---
+++ oop_crud/view/login.php
@@ -0,0 +1,33 @@
+<?php 
+include '../controller/Auth.php';
+
+$ctrl = new Auth();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login Apps</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+</head>
+
+<body>
+    <section class="login-dark">
+        <form method="post" action="<?php echo $ctrl->login(); ?>">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+            <div class="form-group"><input class="form-control" type="text" name="user" placeholder="Username"></div>
+            <div class="form-group"><input class="form-control" type="password" name="pass" placeholder="Password"></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="login">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a>
+        </form>
+    </section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
